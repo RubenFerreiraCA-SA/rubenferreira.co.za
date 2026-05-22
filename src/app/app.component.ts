28abea2f0d5e38095e03e94240eaffcff721b822
FerricTech <ferric.tech@gmail.com>
2026-05-23 01:39:24 +0200

    Landing page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [RouterOutlet, LandingPageComponent],
+  template: `<app-landing-page></app-landing-page
+    ><router-outlet></router-outlet>`,
 })
 export class AppComponent {
   title = 'rubenferreira.co.za';
