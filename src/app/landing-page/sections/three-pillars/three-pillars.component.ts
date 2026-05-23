422122690c938b8070b0bd9763159a0e4e4fb0ca
FerricTech <ferric.tech@gmail.com>
2026-05-23 12:48:02 +0200

    Three Pilars Added

---
+++ src/app/landing-page/sections/three-pillars/three-pillars.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three-pillars',
+  imports: [],
+  templateUrl: './three-pillars.component.html',
+  styleUrl: './three-pillars.component.scss'
+})
+export class ThreePillarsComponent {
+
+}
