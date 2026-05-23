36d0e2cb34cde93096ed0a125b368071d3aad5c4
Ruben <rubenf85@gmail.com>
2026-05-23 23:13:38 +0200

    Recent Writing added

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeroCardComponent } from './sections/hero-card/hero-card.component';
 import { ThreePillarsComponent } from './sections/three-pillars/three-pillars.component';
 import { FeaturedResearch } from './sections/featured-research/featured-research';
+import { RecentWritings } from './sections/recent-writings/recent-writings';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  imports: [HeroCardComponent, ThreePillarsComponent, FeaturedResearch],
+  imports: [HeroCardComponent, ThreePillarsComponent, FeaturedResearch, RecentWritings],
 })
 export class LandingPage { }
---
+++ src/app/landing-page/sections/recent-writings/recent-writings.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-writings',
+  imports: [],
+  templateUrl: './recent-writings.html',
+  styleUrl: './recent-writings.scss',
+})
+export class RecentWritings {
+
+}
