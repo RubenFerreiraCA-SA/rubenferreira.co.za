2fd028fbf6c2958108876791686f55aae813a374
Ruben <rubenf85@gmail.com>
2026-05-23 22:44:10 +0200

    Featured Research section added and styling improved

--- src/app/landing-page/sections/featured-research/featured-research.ts
+++ src/app/landing-page/sections/featured-research/featured-research.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-featured-research',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './featured-research.html',
   styleUrl: './featured-research.scss',
 })
 export class FeaturedResearch {
+  selectedChip: string | null = null;
 
+  selectChip(chip: string): void {
+    this.selectedChip = this.selectedChip === chip ? null : chip;
+  }
 }
