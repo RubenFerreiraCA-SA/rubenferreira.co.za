76a0a4a433dafb4107b8a4e72718a2578d799a21
Ruben <rubenf85@gmail.com>
2026-05-23 22:25:02 +0200

    Add Featured Research section to landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroCardComponent } from './sections/hero-card/hero-card.component';
 import { ThreePillarsComponent } from './sections/three-pillars/three-pillars.component';
+import { FeaturedResearch } from './sections/featured-research/featured-research';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  imports: [HeroCardComponent, ThreePillarsComponent],
+  imports: [HeroCardComponent, ThreePillarsComponent, FeaturedResearch],
 })
-export class LandingPage {}
+export class LandingPage { }
---
+++ src/app/landing-page/sections/featured-research/featured-research.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-research',
+  imports: [],
+  templateUrl: './featured-research.html',
+  styleUrl: './featured-research.scss',
+})
+export class FeaturedResearch {
+
+}
