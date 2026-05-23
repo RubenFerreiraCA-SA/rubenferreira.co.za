df114dd5c57f7ef382e6ee3b882f08ef354b2636
FerricTech <ferric.tech@gmail.com>
2026-05-23 11:52:42 +0200

    Pivot page design

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroCardComponent } from './sections/hero-card/hero-card.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
+  imports: [HeroCardComponent],
 })
 export class LandingPage {}
