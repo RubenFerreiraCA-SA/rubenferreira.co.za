5425bafa88169ae7c5605312686a23db283a1311
Ruben <rubenf85@gmail.com>
2026-05-23 21:24:31 +0200

    Three pillars section added

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroCardComponent } from './sections/hero-card/hero-card.component';
+import { ThreePillarsComponent } from './sections/three-pillars/three-pillars.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  imports: [HeroCardComponent],
+  imports: [HeroCardComponent, ThreePillarsComponent],
 })
 export class LandingPage {}
